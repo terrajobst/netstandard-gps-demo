a06e8bfdcb07909dd22d92a947d316611d886535
Immo Landwerth <immol@microsoft.com>
2017-04-27 09:27:54 -0700

    Initial commit

---
+++ App_Net461/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using Gps;
+
+namespace App_Net461
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var geo = await GpsLocation.GetCoordinates();
+
+            label1.Text = $"You're at ({geo.latitude:N2}, {geo.longitude:N2})";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
